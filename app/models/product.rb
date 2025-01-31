78d379398f12f69a87dc06f4af7ac7aa8bfba461
tchering <sonam.sherpa.lama@gmail.com>
2025-02-01 00:08:42 +0100

    defined relationship in user model and added call back method before cart deletion

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,21 @@ class Product < ApplicationRecord
                           message: "must be a URL for GIF, JPG or PNG image.",
                         }
   validates :price, numericality: { greater_than_or_equal_to: 0.01, message: "must be at least 0.01" }
+
+  has_many :cart_items, dependent: :destroy
+  before_destroy :ensure_not_referenced_by_any_cart_item
+
+  private
+
+  def ensure_not_referenced_by_any_cart_item
+    unless cart_items.empty?
+      errors.add(:base, "Items are still in cart. Remove them first.")
+      throw :abort
+    end
+  end
+
+  #:base is a special key used in Rails' errors object to add errors that apply to the entire object, rather than to a specific attribute.In this casse Product.
 end
+
+# Here we declare that a product has many cart_items and define a hook method named ensure_not_referenced_by_any_cart_item that is called before the destroy action.
+#! A hook method(call back) is a method whose name starts with "before_" and is called before a specific action.
