47018dc4ea0cdf2953c1f4a62bb641f590c7836d
tchering <sonam.sherpa.lama@gmail.com>
2025-01-31 23:46:27 +0100

    some comment on set_cart

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -2,7 +2,7 @@ module CurrentCart
   private
 
   def set_cart
-    @cart = Cart.find(session[:cart_id]) if session[:cart_id]
+    @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
     @cart = Cart.create
     session[:cart_id] = @cart.id
