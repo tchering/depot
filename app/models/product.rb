bd0dbefadd514f6df1bedf1f4e5d818c62e4c111
tchering <sonam.sherpa.lama@gmail.com>
2025-01-31 15:32:01 +0100

    validation rspec test in for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
-  validates :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :description, presence: true, length: { minimum: 5, maximum: 500, message: "should be between 5 and 500 characters" }
+  validates :image_url, presence: true, allow_blank: true, format: {
+                          with: %r{\.(gif|jpg|png)\Z}i,
+                          message: "must be a URL for GIF, JPG or PNG image.",
+                        }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01, message: "must be at least 0.01" }
 end
--- spec/model/product_spec.rb
+++ spec/model/product_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Product, type: :model do
     expect(product).to be_valid
   end
 
-  it "is invalid with missing attributes" do
-    product = Product.create(title: "hello", description: "testing product creation", image_url: "",
-                             price: "19.19")
+  it "is invalid with duplicate title" do
+    Product.create(title: "hello", description: "testing product creation", image_url: "", price: "19.19")
+    product = Product.new(title: "hello", description: "another product", image_url: "", price: "19.19")
     expect(product).not_to be_valid
   end
 
@@ -33,5 +33,37 @@ RSpec.describe Product, type: :model do
     )
     product.destroy
     expect(Product.find_by(id: product.id)).to be_nil
+  end
+
+  it "image url should contain the extension" do
+    product = Product.create!(
+      title: "Original Book",
+      description: "Initial description",
+      image_url: "cover.jpg",
+      price: 29.99,
+    )
+    expect(product.image_url).to match(/\.(jpg|png|gif)\z/i)
+  end
+
+  it " should not be valid with missing extension for image url" do
+    product = Product.new(
+      title: "Original Book",
+      description: "Initial description",
+      image_url: "cover",
+      price: 29.99,
+    )
+    expect(product).not_to be_valid
+    expect(product.errors[:image_url]).to include("must be a URL for GIF, JPG or PNG image.")
+  end
+
+  it "should not be valid description with min length of 5 and max length of 255" do
+    product = Product.new(
+      title: "hello",
+      description: "a",
+      image_url: "cover.jpg",
+      price: 29.99,
+    )
+    expect(product).not_to be_valid
+    expect(product.errors[:description]).to include("should be between 5 and 500 characters")
   end
 end
